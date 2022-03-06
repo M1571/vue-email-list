9cb73b1c3472e17c408d00252536c0e8d9faecdf
M1571 <94639639+M1571@users.noreply.github.com>
2022-03-06 20:02:50 +0100

    set axios

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 // console.log('prova')
 
 // -----------------------------
+
+// Attraverso l’apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail
+// chiedere 10 indirizzi email e stamparli in pagina all’interno di una lista.
+// Successivamente assicuratevi di far comparire gli indirizzi email solamente quando sono stati tutti recuperati dal server.
+
+// -----------------------------
+// START
+
+// console.log(axios);
+
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+ .then(function (response) {
+    // const result = response.data;
+    console.log(response);
+});
